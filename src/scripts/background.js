ab2a176afb8e6fd5407bc4ea2389be762f6d4211
webberg <webbery@foxmail.com>
2021-04-08 23:20:35 +0800

    fixed build error

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -1,12 +1,12 @@
 import ext from "./utils/ext";
-// import nanomsg from 'nanomsg';
+
+var connection = new WebSocket('ws://127.0.0.1:8013', 'civet-protocol');
 
 ext.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if(request.action === "perform-save") {
       console.log("Extension Type: ", "/* @echo extension */");
       console.log("PERFORM AJAX", request.data);
-
       sendResponse({ action: "saved" });
     }
   }
